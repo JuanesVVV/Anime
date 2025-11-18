a527279c20b616b116d6f4dd19c2693b31843948
Juan Esteban <102327436+JuanesVVV@users.noreply.github.com>
2025-11-18 22:22:55 +0000

    Revert "ACTUALIZACION"
    This reverts commit a7a721caffe2c421cf6478e6e7a1c9687608e093.

--- proyecto/src/firebaseConfig.js
+++ proyecto/src/firebaseConfig.js
@@ -1,7 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-
 const firebaseConfig = {
   apiKey: "AIzaSyDCKG2Z-kx6cput7TXsLereA741k1ENxBg",
   authDomain: "anime-add54.firebaseapp.com",
@@ -11,9 +10,8 @@ const firebaseConfig = {
   appId: "1:576239763868:web:3132a0bb9e5195aa8b4859",
   measurementId: "G-6KVN00S4JG"
 };
-
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const db = getFirestore(app);
 
+const db = getFirestore(app); // ✅ ¡Esto es necesario!
 export { auth, db };
